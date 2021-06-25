fc839543640f246773fcbe5f02f7d6dbfefa156e
Hunterttt <tianyuan.ttt@gmail.com>
2021-06-25 16:41:59 +0900

    fdfd

--- py_tian03_jumpssh/ijumpssh.py
+++ py_tian03_jumpssh/ijumpssh.py
@@ -1,7 +1,7 @@
 from jumpssh import SSHSession
 
 # establish ssh connection between your local machine and the jump server
-gateway_session = SSHSession('192.168.32.101',username='adminssss',password='123.comcfdfd').open()
+gateway_session = SSHSession('192.168.32.101',username='admin',password='123.com').open()
 
 # from jump server, establish connection with a remote server
 remote_session = gateway_session.get_remote_session('12.1.1.2',username='cisco',password='123.com')
